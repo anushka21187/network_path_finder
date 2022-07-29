bc0748d87a4c709052f999a5a7352a9616deb33f
anushka21187 <90597061+anushka21187@users.noreply.github.com>
2022-07-29 17:03:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import network_matrix
 import available_ports
+import obtain_paths
 
-mesh = network_matrix(4, 4)
+# 2x3 matrix
+number_of_rows = 2
+number_of_columns = 3
+
+source = 0
+destination = 2
+
+mesh = network_matrix(number_of_rows, number_of_columns)
 port_map_mesh = available_ports('mesh', mesh)
-port_map_torus = available_ports('torus', mesh)
+#port_map_torus = available_ports('torus', mesh)
+
+empty_dict = {}
+empty_list = []
+source_directions = port_map_mesh[source]
+
+dictionary_of_paths = obtain_paths(port_map_mesh, number_of_rows, number_of_columns, source, destination, empty_list, source_directions, empty_dict)

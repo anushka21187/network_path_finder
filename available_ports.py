f30b0b264f3496e56b006a7a50fa12b518582a22
anushka21187 <90597061+anushka21187@users.noreply.github.com>
2022-07-29 23:14:16 +0530

    Update available_ports.py

--- available_ports.py
+++ available_ports.py
@@ -1,9 +1,9 @@
 import network_matrix
 
 """
-Map the nodes in the router network to a list of available ports
-Input: topology (string), network (can be a 2D list obtained from network_matrix)
-Output: A dictionary with key as a node id and value as the list of directions allowed for packet movement from that node
+Map the nodes in the router network to a list/tuple of available ports
+Input: topology (string), network (can be a 2D tuple or list obtained from network_matrix)
+Output: A dictionary with key as a node id and value as the list/tuple of directions allowed for packet movement from that node
 The topology determines which directions are allowed.
 In torus topology, all directions are allowed in each node, so direction-related information becomes irrelevant. This is not the case for mesh topology.
 """
@@ -16,8 +16,7 @@ def available_ports(topology, network):
         list_of_directions = ['N', 'S', 'W', 'E']
         for row_index in range (0, len(network)):
             for col_index in range (0, len(network[row_index])):
-                available_directions[network[row_index][col_index]] = \
-                list_of_directions
+                available_directions[network[row_index][col_index]] = list_of_directions
                 
     elif topology=='mesh':
         for row_index in range (0, len(network)):
@@ -44,26 +43,20 @@ def available_ports(topology, network):
                     # then packets can travel east but not west
                     # so add east to the list
                     list_of_directions.append('E')
-                    available_directions[network[row_index][col_index]] = []
-                    for i in range (0, len(list_of_directions)):
-                        available_directions[network[row_index][col_index]].append(list_of_directions[i])
+                    available_directions[network[row_index][col_index]] = tuple(list_of_directions)
                     del(list_of_directions[-1])   
                     
                 # or if this is the last column ...
                 elif col_index==len(network[row_index])-1:
                     # then packets can only travel west but not east
                     list_of_directions.append('W')
-                    available_directions[network[row_index][col_index]] = []
-                    for i in range (0, len(list_of_directions)):
-                        available_directions[network[row_index][col_index]].append(list_of_directions[i])
+                    available_directions[network[row_index][col_index]] = tuple(list_of_directions)
                     del(list_of_directions[-1]) 
                 # or if this node is in any other column ... 
                 else:
                     # add west and east to the list
                     list_of_directions.extend(['W', 'E'])
-                    available_directions[network[row_index][col_index]] = []
-                    for i in range (0, len(list_of_directions)):
-                        available_directions[network[row_index][col_index]].append(list_of_directions[i])
+                    available_directions[network[row_index][col_index]] = tuple(list_of_directions)
                     del(list_of_directions[-2:])
         
     return available_directions

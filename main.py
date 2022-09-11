a791c4a2fb3b212b51034deaee6f36151906c293
anushka21187 <90597061+anushka21187@users.noreply.github.com>
2022-09-11 17:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ west = 'westbits'
 east = 'eastbits'
 
 mesh_size_var = anushka_1
-num_sources_var = anushka_s
+sources_var = anushka_s
 mode_var = anushka_m
 report_format_var = anushka_rf
 
@@ -25,11 +25,19 @@ if 'reports' not in os.listdir():
     os.mkdir('reports')
 
 
-if num_sources_var == 2:
-    generate_routepairs_s2(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
-elif num_sources_var == 1:
-    generate_routepairs(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
+if sources_var == 2:
+    if report_format_var != 3:
+        generate_routepairs_s2(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
+    else:
+        generate_routepairs_concise_s2(anushka_1, anushka_1, mode_var, north, south, west, east)
+elif sources_var == 1:
+    if report_format_var != 3:
+        generate_routepairs(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
+    else:
+        generate_routepairs_concise(anushka_1, anushka_1, mode_var, north, south, west, east)
 else:
-    generate_routepairs_s2all(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
+    if report_format_var != 3:
+        generate_routepairs_s2all(anushka_1, anushka_1, mode_var, report_format_var, north, south, west, east)
+    else:
+        generate_routepairs_concise_s2all(anushka_1, anushka_1, mode_var, north, south, west, east)
 
-    
